72aa964cdca451e1b091814aecf4fbe3c76b9968
idrhee <inbok.rhee@kdis.ac.kr>
2023-02-15 02:25:10 +0900

    links

--- docs/export_cv.R
+++ docs/export_cv.R
@@ -1,7 +1,7 @@
 pagedown::find_chrome()
-options(pagedown.remote.maxattempts=10) # number of attempt in total
+options(pagedown.remote.maxattempts=1000) # number of attempt in total
 options(pagedown.remote.sleeptime=2) # time in second between attempt
-pagedown::chrome_print("index.html",verbose = TRUE)
-pdftools::pdf_subset(input = "index.pdf", pages =  2:11)
-fs::file_copy(path = "index_output.pdf", new_path = "index.pdf", overwrite = TRUE)
+pagedown::chrome_print("docs/index.html")
+pdftools::pdf_subset(input = "docs/index.pdf", pages =  2:11)
+fs::file_copy(path = "docs/index_output.pdf", new_path = "docs/index.pdf", overwrite = TRUE)
 
